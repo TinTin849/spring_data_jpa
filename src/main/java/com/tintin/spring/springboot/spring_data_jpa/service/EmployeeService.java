bb5544b014887f41a2340393a2d275c001599fc6
Anton Lugovoy <tintin200227@gmail.com>
2022-03-30 16:37:46 +0300

    Initial commit

---
+++ src/main/java/com/tintin/spring/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.tintin.spring.springboot.spring_data_jpa.service;
+
+import com.tintin.spring.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+    public List<Employee> findAllByName(String name);
+}
